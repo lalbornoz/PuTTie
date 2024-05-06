2b8d8196b001a65f51ad9f3ec0612c6374946d89
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2024-05-06 21:20:32 +0200

    PuTTie: implements putty.exe command line arguments -?, -h, and --help (fixes #13.)

---
+++ PuTTie/winfrip_subr_putty_help.h
@@ -0,0 +1,19 @@
+/*
+ * winfrip_subr_putty_help.h - pointless frippery & tremendous amounts of bloat
+ * Copyright (c) 2018, 2022, 2023 Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
+ */
+
+#ifndef PUTTY_WINFRIP_SUBR_PUTTY_HELP_H
+#define PUTTY_WINFRIP_SUBR_PUTTY_HELP_H
+
+/*
+ * Public subroutine prototypes private to windows/putty.c
+ */
+
+void WfPuttyCmdLineHelp(const char *cmdline, bool *pexitfl, int *pexit_status);
+
+#endif // !PUTTY_WINFRIP_SUBR_PUTTY_HELP_H
+
+/*
+ * vim:noexpandtab sw=8 ts=8 tw=0
+ */
